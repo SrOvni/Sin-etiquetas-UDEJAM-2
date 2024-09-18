228e36d2e55a6119a772af06be3baf8e73978290
Renier Gaytan <renier.gaytan@gmail.com>
2024-09-18 00:50:28 -0600

    Pixxe art prota y cordi

---
+++ Assets/Scripts/Renier/SignTypeScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum SignType
+{
+    Hola
+}
+public class SignTypeScriptableObject : ScriptableObject
+{
+    
+}
