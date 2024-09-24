8e231b4eea06f6dd373904c672b9e0b5b0e4cc2e
williamjair11 <o.c.william.jair@gmail.com>
2024-09-23 21:05:37 -0600

    dummy

--- Assets/Scripts/EndTheGame.cs
+++ Assets/Scripts/EndTheGame.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EndTheGame : MonoBehaviour
 {   
+    GameManager gameManager;
+
+    [SerializeField] private UnityEvent _musicFinal;
+    private void Start()
+    {
+        gameManager = GetComponent<GameManager>();
+    }
     public IEnumerator StarEndTheGame()
     {
+        _musicFinal.Invoke();
         yield return new WaitForSeconds(30f);
-        //Cargar Menu Principal
+        gameManager.ChangeSceneByName("MenuPrincipal");
     }
 }
